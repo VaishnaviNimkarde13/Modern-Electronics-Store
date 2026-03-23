1d1f093f81107f728de6bc99403036f283ee0c7f
VaishnaviNimkarde13 <vaishnavi.nimkarde@avotechsystems.com>
2026-03-23 17:54:41 +0530

    commit

--- Modern-Electronic-Store/src/App.jsx
+++ Modern-Electronic-Store/src/App.jsx
@@ -1,28 +1,24 @@
-
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AuthPage from './Pages/Login'
-// import Home     from './Pages/Home'
-import Services from './Pages/Services'
-import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthPage from "./Pages/Login";
+import Home from "./Pages/Home";
+import Services from "./Pages/Services";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-          <Navbar />
+      <Navbar />
 
       <Routes>
-        {/* <Route path="/"         element={<Home />}     /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/login"    element={<AuthPage />} />
-        <Route path="/signup"   element={<AuthPage defaultTab="signup" />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/signup" element={<AuthPage defaultTab="signup" />} />
       </Routes>
-            <Footer />
-
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
-
+export default App;
